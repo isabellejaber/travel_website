259c8ad41c86382abb61713e3c349879a97008fa
EC2 Default User <ec2-user@ip-172-31-21-36.us-east-2.compute.internal>
2019-07-19 15:00:53 +0000

    Button and styling

--- app/routes.py
+++ app/routes.py
@@ -30,8 +30,9 @@ def rome():
         walking = (user["walking"]).decode('utf-8')
         crowds = (user["crowds"]).decode('utf-8')
         rome_activity_generator = model.rome_activity(activity, walking, crowds)
+        pic = model.image(activity)
         print(rome_activity_generator)
-        return render_template('rome.html', rome_activity_generator = rome_activity_generator)
+        return render_template('rome.html', rome_activity_generator = rome_activity_generator, pic = pic)
 
 @app.route('/brussels', methods = ["GET", "POST"])
 def brussels():
@@ -43,5 +44,6 @@ def brussels():
         walking = (user["walking"]).decode('utf-8')
         crowds = (user["crowds"]).decode('utf-8')
         brussels_activity_generator = model.brussels_activity(activity, walking, crowds)
+        pic = model.image(activity)
         print(brussels_activity_generator)
-        return render_template('brussels.html', brussels_activity_generator = brussels_activity_generator)
+        return render_template('brussels.html', brussels_activity_generator = brussels_activity_generator, pic = pic)
